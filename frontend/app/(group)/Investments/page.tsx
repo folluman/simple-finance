2c08b8cb2386e8b94ddc371a4083051ec2162bfa
Guilherme Carvalho <guicarvcontato@outlook.com>
2026-06-06 15:18:06 -0300

    fix: correção de cores no header pagina investimentos, alteração de imagens no menu e correção do scroll da pagina de investimentos

--- frontend/app/(group)/Investments/page.tsx
+++ frontend/app/(group)/Investments/page.tsx
@@ -20,28 +20,29 @@ const monthlyInvestments = [
 
 export default function Investments() {
   return (
-    <div className="w-full p-4 flex flex-col gap-6 md:p-8 xl:h-full">
-      <header className="flex flex-col items-center w-full gap-4 p-5 bg-tertiary-color-green rounded-2xl shadow-md md:items-center md:p-6 md:flex-row md:justify-between md:gap-6">
+    <div className="w-full h-full p-4 flex flex-col gap-6 md:p-8 overflow-y-auto overflow-x-hidden">
+      
+      <header className="flex flex-col items-center w-full gap-4 p-5 bg-line-gray rounded-2xl shadow-md md:items-center md:p-6 md:flex-row md:justify-between md:gap-6 shrink-0">
         <div className="flex flex-col md:w-4/6">
           <h2 className="text-primary-color-green font-bold text-2xl text-center md:text-left md:text-3xl">
             Investimentos
           </h2>
-          <p className="text-zinc-500 mt-1 text-sm">
+          <p className="text-primary-color-green mt-1 text-sm text-center md:text-left">
             Análise detalhada dos seus investimentos.
           </p>
         </div>
-        <div className="flex flex-col gap-4 w-full md:2/6 lg:flex-row lg:justify-end">
-          <button className="bg-primary-color-green p-3 md:px-6 rounded-xl flex items-center justify-center gap-3 text-secondary-color-green font-semibold text-sm w-full md:w-auto hover:opacity-90 transition-opacity cursor-pointer">
+        <div className="flex flex-col gap-4 w-full md:w-2/6 lg:flex-row lg:justify-end">
+          <button className="bg-primary-color-green p-3 md:px-6 rounded-xl flex items-center justify-center gap-3 text-secondary-color-green font-semibold text-sm w-full lg:w-auto hover:opacity-90 transition-opacity cursor-pointer">
             <Image
               src="/img-add.png"
-              alt="Ícone adicionar gasto"
-              title="Clique aqui para adicionar gasto"
+              alt="Ícone adicionar investimento"
+              title="Clique aqui para adicionar investimento"
               height={20}
               width={20}
             />
-            Adicionar investimento
+            Adicionar
           </button>
-          <button className="bg-primary-color-green p-3 md:px-6 rounded-xl flex items-center justify-center gap-3 text-secondary-color-green font-semibold text-sm w-full md:w-auto hover:opacity-90 transition-opacity cursor-pointer">
+          <button className="bg-primary-color-green p-3 md:px-6 rounded-xl flex items-center justify-center gap-3 text-secondary-color-green font-semibold text-sm w-full lg:w-auto hover:opacity-90 transition-opacity cursor-pointer">
             <Image
               src="/img-launch.png"
               alt="Ícone novo lançamento"
@@ -53,17 +54,20 @@ export default function Investments() {
           </button>
         </div>
       </header>
-      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-6">
+
+      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-6 shrink-0">
         <CardFinance value="R$ 13.525,41" title="Rico" />
         <CardFinance value="R$ 2.346,71" title="Binance" />
         <CardFinance value="R$ 10.671,24" title="XP Investimentos" />
         <CardFinance value="R$ 60.671,24" title="Nubank" />          
       </section>
-      <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 xl:flex-1 xl:min-h-0">
+
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 xl:flex-1 xl:min-h-75 w-full shrink-0">
         <CustomPieChart title="Investimentos atuais" data={investmentData} />
         <YieldBarChart />
       </section>
-      <section className="xl:flex-1 xl:min-h-0">
+
+      <section className="xl:flex-1 xl:min-h-75 w-full shrink-0 overflow-x-auto pb-2">
           <CustomBarChart 
             title="Total Investido por mês"
             tooltipLabel="Total Investido"
